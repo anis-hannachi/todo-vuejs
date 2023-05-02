242ea7ff923ab41d8b030b67740e0b814f665e6a
Anis Hannachi <hannachi.anis@gmail.com>
2023-05-02 02:31:53 +0200

    todo app

---
+++ src/views/AboutView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="about">
+    <h1>Todos</h1>
+    <p style="padding-top: 3%">
+      Vue Todos est une application CRUD de suivi des tâches créée avec Vue 3
+      Suivez vos tâches quotidiennes en utilisant cette application de suivi des
+      tâches qui permet de sauvegarder, compléter, éditer et supprimer les
+      tâches. On peut ajouter à cette application des fonctionnalités telles que
+      la possibilité de faire glisser et de réorganiser les tâches, d'animer la
+      création/suppression des tâches ou même de créer une application complète
+      (Full stack) avec la possibilité d'avoir différentes listes pour
+      différents jours de suivi des tâches !
+    </p>
+  </div>
+</template>
+
+<style>
+.about {
+  display: flex;
+  flex-direction: column;
+  margin: 10%;
+}
+</style>
